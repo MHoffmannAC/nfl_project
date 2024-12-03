f544116b36b912dd8bf5c58bba2890ceb95561b1
MHoffmannAC <mhoffmann.aachen@gmail.com>
2024-12-03 18:27:50 +0100

    Update prediction.py

--- streamlit/prediction.py
+++ streamlit/prediction.py
@@ -68,7 +68,6 @@ st.write("...")
 if "game" in st.query_params:
     st.session_state["choice"] = "Live Game"
     st.session_state["game_name_selected"] = st.query_params.game
-    st.query_params = {}
 
 if "choice" not in st.session_state:
     st.session_state["choice"] = None
