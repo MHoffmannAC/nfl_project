09a0304493990a0bced71cd3dbf4e958f7dcea28
Markus Hoffmann <mhoffmann.aachen@gmail.com>
2025-05-31 13:20:40 +0200

    meme fixes?

--- streamlit/memes.py
+++ streamlit/memes.py
@@ -1,21 +1,25 @@
 import streamlit as st
-import instaloader
-from groq import Groq
-import requests
+from streamlit_server_state import server_state, server_state_lock, no_rerun
 
-def fetch_latest_images(username, X=5):
-    L = instaloader.Instaloader()
-    profile = instaloader.Profile.from_username(L.context, username)
-    latest_images = []
-    for post in profile.get_posts():
-        if len(latest_images) < X:
-            latest_images.append({
-                'image_url': post.url,          # Direct image URL
-                'caption': post.caption         # Caption of the post
-            })
-        else:
-            break
-    return latest_images
+import time
+import requests
+from groq import Groq
+
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
+from webdriver_manager.core.os_manager import ChromeType
+
+from bs4 import BeautifulSoup
+
+st.title("Memes from nflmemes_ig 'explained' by an AI")
+st.subheader("We let an AI 'explain' to us the latest memes from nflmemes_ig. Let's see whether artificial intelligence gets the jokes...")
+
+if not "memes" in server_state:
+    with no_rerun:
+        with server_state_lock["memes"]:
+            server_state["memes"] = []
 
 def get_image_caption(image_url, caption):
     client = Groq(api_key=st.secrets['GROQ_TOKEN'])
@@ -38,56 +42,18 @@ def get_image_caption(image_url, caption):
     )
     return completion.choices[0].message.content
 
-st.title("Memes from nflmemes_ig 'explained' by an AI")
-st.subheader("We let an AI 'explain' to us the latest memes from nflmemes_ig. Let's see whether artificial intelligence gets the jokes...")
-
-latest_images = []
-#latest_images = fetch_latest_images('nflmemes_ig', X=10)
-
-
-for post in latest_images:
-    image_url = post['image_url']
-    caption = post['caption']
-    cols = st.columns(2)
-
-    with cols[0]:
-        response = requests.get(image_url)
-        if response.status_code == 200:
-            st.image(response.content, width=300)  # Pass the raw image content
-        else:
-            st.error("Failed to load image")
-        st.text(caption)
-    with cols[1]:
-        caption = get_image_caption(image_url, caption)
-        st.write(caption)
-    st.divider()
-
-
-
-from bs4 import BeautifulSoup
-import time
-import requests
-from streamlit_server_state import server_state, server_state_lock, no_rerun
-
-from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.chrome.service import Service
-from webdriver_manager.chrome import ChromeDriverManager
-from webdriver_manager.core.os_manager import ChromeType
-
-@st.cache_resource
+@st.cache_resource(show_spinner=False)
 def get_driver():
     return webdriver.Chrome(
         service=Service(
             ChromeDriverManager(
-                #chrome_type=ChromeType.CHROMIUM
+                chrome_type=ChromeType.CHROMIUM
                 ).install()
         ),
         options=options,
     )
 
 try:
-
     options = Options()
     options.add_argument("--disable-gpu")
     options.add_argument("--headless")
@@ -107,37 +73,27 @@ try:
 
     soup = BeautifulSoup(driver.page_source, "html.parser")
     driver.quit()
-except:
-    soup = BeautifulSoup("", "html.parser")
+    
+    for container in soup.find_all("div", class_="_aagv"):
+        img = container.find("img")
+        if img and img.get("src"):
+            src = img["src"]
+            alt = img.get("alt", "")
+            if not src in [i['src'] for i in server_state["memes"]]:
+                with no_rerun:
+                    with server_state_lock["memes"]:
+                        server_state["memes"].append({"src": src, "alt": alt, "ai": ""})
+except Exception as e:
+    driver = get_driver()
+    driver.quit()
+    print(e)
 
-if not "memes" in server_state:
-    with no_rerun:
-        with server_state_lock["memes"]:
-            server_state["memes"] = []
-
-for container in soup.find_all("div", class_="_aagv"):
-    img = container.find("img")
-    if img and img.get("src"):
-        src = img["src"]
-        alt = img.get("alt", "")
-        if not src in [i['src'] for i in server_state["memes"]]:
-            with no_rerun:
-                with server_state_lock["memes"]:
-                    server_state["memes"].append({"src": src, "alt": alt, "ai": ""})
-
-#with no_rerun:
-#    with server_state_lock["memes"]:    
-#        server_state["memes"] = server_state["memes"][:10]
-
-
-for i in server_state['memes'][-10:]:
-    print(i)
-    print(i.get('ai', ""))
-    if i.get('ai', "") == "":
-        i['ai'] = get_image_caption(i['src'], i['alt'])
-
+for i in range(10):
+    if server_state['memes'][-i].get('ai', "") == "":
+        server_state['memes'][-i]['ai'] = get_image_caption(server_state['memes'][-i]['src'], server_state['memes'][-i]['alt'])
     st.divider()        
     cols = st.columns([1,2])
-    img_data = requests.get(i['src']).content
+    img_data = requests.get(server_state['memes'][-i]['src']).content
     cols[0].image(img_data, width=300)
-    cols[1].text(i['ai'])
+    cols[1].text(server_state['memes'][-i]['ai'])
+    
