9c9f50c04b05edc44d4a116b243f2e3f57e34a81
Markus Hoffmann <mhoffmann.aachen@gmail.com>
2025-10-25 22:39:22 +0200

    fix: Perform ruff on streamlit/games.py

--- streamlit/games.py
+++ streamlit/games.py
@@ -1,12 +1,20 @@
-import streamlit as st
+from sources import hangman, pixellogo, ttt
 from streamlit.components.v1 import iframe
-import sources.hangman as hangman
-import sources.pixellogo as pixellogo
-import sources.ttt as ttt
+
+import streamlit as st
 
 st.title("Collection of NFL-themed Games", anchor=False)
 
-game = st.segmented_control("Select a game", ["Tic-Tac-Toe (1 vs. 1)", "Hangman (Singleplayer)", "Sudoku (Singleplayer)", "PixelLogos (Multiplayer)"],  default=None)
+game = st.segmented_control(
+    "Select a game",
+    [
+        "Tic-Tac-Toe (1 vs. 1)",
+        "Hangman (Singleplayer)",
+        "Sudoku (Singleplayer)",
+        "PixelLogos (Multiplayer)",
+    ],
+    default=None,
+)
 
 if game == "Sudoku (Singleplayer)":
     st.header("2025", anchor=False)
@@ -15,22 +23,21 @@ if game == "Sudoku (Singleplayer)":
     st.header("2024", anchor=False)
     iframe("https://sudokupad.app/xsk7whp4qz", height=800)
 elif game == "Hangman (Singleplayer)":
-    hangman.run_game()      
+    hangman.run_game()
 elif game == "PixelLogos (Multiplayer)":
     pixellogo.initialize_game()
     pixellogo.display_game()
-elif game=="Tic-Tac-Toe (1 vs. 1)":
+elif game == "Tic-Tac-Toe (1 vs. 1)":
     ttt.run_game()
 else:
     st.header("Please select a game from the choices above", anchor=False)
     st.write("""**Available games**:
- 
+
  - **`Tic-Tac-Toe`**: Be the first to get three in a row (1 vs. 1)
-             
+
  - **`Hangman`**: The typical hangman game. Guess a hidden word and avoid making mistakes. (Singleplayer)
- 
+
  - **`Sudoku`**: Solve some NFL-themed Sudokus. (Singleplayer)
- 
+
  - **`PixelLogo`**: Guess quicker than your opponents which logo is displayed. (Multiplayer)
 """)
-    
